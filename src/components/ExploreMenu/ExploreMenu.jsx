fc9a2a9b584f29187e483066e9b1cbf39a884f27
Hanzdev <huynhngocanhittt@gmail.com>
2024-06-29 23:26:17 +0700

    Explore Menu

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-
 import './ExploreMenu.css'
+
 import { menu_list } from '../../assets/assets'
 
 const ExploreMenu = (category, setCategory) => {
@@ -10,16 +10,16 @@ const ExploreMenu = (category, setCategory) => {
       <p className='explore-menu-text'>Choose from a diverse menu featuring a delectabel array of dishes. Our mission is to satisfy your craving and elevate your dinning experience, one delicious meal at a time.</p>
       <div className="explore-menu-list">
         {menu_list.map((item, index) => {
+          // console.log(category)
           return (
-            <div onClick={ () => setCategory( prev => prev===item.menu_name?'All':item.menu_name)} key={index} className='explore-menu-list-item'>
-              <img className={category===item.menu_name?'active':''} src={item.menu_image} alt="" />
+            <div onClick={ () => setCategory( a => a === item.menu_name ? 'All' : item.menu_name)} key={index} className='explore-menu-list-item'>
+              <img className={category === item.menu_name ? 'active' : ''} src={item.menu_image} alt="" />
               <p>{item.menu_name}</p>
             </div>
           )
         })}
       </div>
       <hr />
-
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react'
+import './Home.css'
 
 import Header from '~/components/Header/Header'
 import ExploreMenu from '~/components/ExploreMenu/ExploreMenu'
 
-import './Home.css'
 
 const Home = () => {
 
   const [category, setCategory] = useState('All')
-  console.log(category)
-  console.log(setCategory)
+
   return (
     <div>
       <Header/>
